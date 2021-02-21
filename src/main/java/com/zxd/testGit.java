0d757fd2275a32f8d75132fd2cc1a186011e1ae5
FreeGuest0 <1064024659@qq.com>
2021-02-21 16:07:50 +0800

    新建项目

---
+++ src/main/java/com/zxd/testGit.java
@@ -0,0 +1,13 @@
+package com.zxd;
+
+/**
+ * @CLASS_NAME testGit
+ * @CLASS_EXPLAIN 此处填写说明
+ * @Author zxd
+ * @Date 2021/2/21 下午4:00
+ */
+public class testGit {
+    public static void main(String[] args){
+        System.out.println("Hello Git");
+    }
+}
